8ccb5e71cdfd6a262964a4854d5d7d4993545257
Vinay-Pundhir <vinay.pundhir@vamstar.io>
2023-09-19 11:46:51 +0530

    remove unused code

--- core/lexer/operator.py
+++ core/lexer/operator.py
@@ -7,11 +7,3 @@ def extract_operators_and_operands(expression: str, operator_list: list) -> tupl
             operands = [v.strip() for v in expression.split(i)]
             return i, operands
     return None, None
-
-
-# def extract_operators_and_operands(expression:str, operator_list:list)->tuple:
-#     for op in operator_list:
-#         pattern = r"(?![\"\']).[OPERATORS](?![\"\'])"
-#         pattern = pattern.replace("OPERATORS", op)
-#         result = re.split(pattern, expression)
-#         result = [i.strip() for i in result]
